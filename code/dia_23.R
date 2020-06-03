957e29eaccb9ea0dc6701189fc08d20fb669377a
guillems <guillems@ethz.ch>
2020-06-03 16:36:12 +0200

    Día 23

--- code/dia_23.R
+++ code/dia_23.R
@@ -4,7 +4,7 @@ library(viridis)
 # Parametros
 url<-"https://raw.githubusercontent.com/cienciadedatos/datos-de-miercoles/master/datos/2019/2019-07-31/la_casa_de_papel.csv"
 personajes<-c("Tokio","Lisboa","Profesor","Moscú","Berlín","Nairobi","Río","Denver","Estocolmo","Helsinki","Bogotá","Palermo","Marsella")
-twitter_dim<-list(width=unit(13/1.5,"cm"),height=unit(6.5/1.5,"cm"))
+twitter_dim<-list(width=unit(13,"cm"),height=unit(6.5,"cm"))
 
 
 # Cargar datos
@@ -48,9 +48,9 @@ toplot_outer_ring<-toplot_main %>%
 
 # Graficar
 g<-ggplot() +
-  geom_bar(data = toplot_inner_ring,aes(x=5,y=n,fill=personaje),stat = "identity",color = "white",width=1,alpha=0.7) +
-  geom_bar(data = toplot_outer_ring,aes(x=6,y=n,fill=personaje),stat = "identity",color = "white",width=1,alpha=0.7) +
-  geom_text(data=toplot_inner_ring,aes(x=5,y = lab.ypos, label = label),size=2.5,fontface="bold",hjust=0.5) +
+  geom_bar(data = toplot_inner_ring,aes(x=5,y=n,fill=personaje),stat = "identity",color = "white",width=1,alpha=0.6) +
+  geom_bar(data = toplot_outer_ring,aes(x=6,y=n,fill=personaje),stat = "identity",color = "white",width=1,alpha=0.6) +
+  geom_text(data=toplot_inner_ring,aes(x=5,y = lab.ypos, label = label),size=2.3,fontface="bold",hjust=0.5) +
   geom_text(data=toplot_outer_ring,aes(x=6,y = lab.ypos, label = label),size=2,fontface="bold",hjust=0.5) +
   geom_text(aes(x=0,y=0,label=str_wrap("Menciones a cada personaje en La Casa de Papel",30))) +
   coord_polar(theta = "y", start = 0) +
